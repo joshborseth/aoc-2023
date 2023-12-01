5ceb550c03df50768d22744c142040457eb3c46c
joshborseth <joshuaborseth@gmail.com>
2023-12-01 11:05:56 -0800

    finished

--- puzzles/day-1/day-1-a.ts
+++ puzzles/day-1/day-1-a.ts
@@ -1,10 +1,38 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const DIGIT_MAP = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
 export async function day1a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const numbers = data.map((line, i) => {
+    const splitLine = line.split('');
+    const nums = splitLine.filter((num) => {
+      if (!isNaN(parseInt(num))) {
+        return parseInt(num);
+      }
+    });
+    const firstNum = nums[0];
+    const lastNum = nums[nums.length - 1];
+    const firstAndLastNum = `${firstNum}${lastNum}`;
+    return parseInt(firstAndLastNum);
+  });
+
+  const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+  return sum;
 }
 
 const answer = await day1a();
+
 console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
--- puzzles/day-1/day-1-b.ts
+++ puzzles/day-1/day-1-b.ts
@@ -1,10 +1,48 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const DIGIT_MAP = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
 export async function day1b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const numbers = data.map((line, i) => {
+    let newLine = line;
+    Object.keys(DIGIT_MAP).forEach((digit) => {
+      const replaceWith = DIGIT_MAP[digit];
+      newLine = newLine.replaceAll(
+        digit,
+        //hehehehehehehehe
+        `${digit[0]}${replaceWith}${digit[digit.length - 1]}`
+      );
+    });
+    const splitLine = newLine.split('');
+    const nums = splitLine.filter((num) => {
+      if (!isNaN(parseInt(num))) {
+        return parseInt(num);
+      }
+    });
+
+    const firstNum = nums[0];
+    const lastNum = nums[nums.length - 1];
+    const firstAndLastNum = `${firstNum}${lastNum}`;
+    return parseInt(firstAndLastNum);
+  });
+
+  const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+  return sum;
 }
 
 const answer = await day1b();
+
 console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
