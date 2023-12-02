af83f0f9cf670d473dcec4d0730ac308c3d50898
joshborseth <joshuaborseth@gmail.com>
2023-12-02 01:51:15 -0800

    finished day2

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,49 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const RED_CUBES = 12;
+const GREEN_CUBES = 13;
+const BLUE_CUBES = 14;
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const formattedData = data.map((line) => {
+    const splitLine = line.split(';');
+    return splitLine.map((str) => {
+      const splitStr = str.split(',');
+      return splitStr.map((str) => {
+        if (str.includes('Game')) {
+          const splitAgain = str.split('');
+          return splitAgain
+            .splice(splitAgain.indexOf(':') + 1)
+            .join('')
+            .trim();
+        }
+        return str.trim();
+      });
+    });
+  });
+  const possibleGames = formattedData.map((game) => {
+    return game.every((line) => {
+      return line.every((val) => {
+        const numsRegex = /\d+/g;
+        const num = Number(val.match(numsRegex));
+        if (val.includes('red')) {
+          if (num > RED_CUBES) return false;
+        } else if (val.includes('green')) {
+          if (num > GREEN_CUBES) return false;
+        } else if (val.includes('blue')) {
+          if (num > BLUE_CUBES) return false;
+        }
+        return true;
+      });
+    });
+  });
+
+  let count = 0;
+  possibleGames.forEach((game, i) => {
+    if (game) count += i + 1;
+  });
+  return count;
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -3,7 +3,48 @@ import chalk from 'chalk';
 
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const formattedData = data.map((line) => {
+    const splitLine = line.split(';');
+    return splitLine.map((str) => {
+      const splitStr = str.split(',');
+      return splitStr.map((str) => {
+        if (str.includes('Game')) {
+          const splitAgain = str.split('');
+          return splitAgain
+            .splice(splitAgain.indexOf(':') + 1)
+            .join('')
+            .trim();
+        }
+        return str.trim();
+      });
+    });
+  });
+  const minRequiredCubes = formattedData.map((game) => {
+    let redMax = 0;
+    let greenMax = 0;
+    let blueMax = 0;
+    game.forEach((val) => {
+      val.forEach((val) => {
+        const numsRegex = /\d+/g;
+        const num = Number(val.match(numsRegex));
+        if (val.includes('red')) {
+          if (num > redMax) redMax = num;
+        } else if (val.includes('green')) {
+          if (num > greenMax) greenMax = num;
+        } else if (val.includes('blue')) {
+          if (num > blueMax) blueMax = num;
+        }
+      });
+    });
+    return [redMax, greenMax, blueMax];
+  });
+
+  let total = 0;
+
+  minRequiredCubes.forEach((game) => {
+    total += game[0] * game[1] * game[2];
+  });
+  return total;
 }
 
 const answer = await day2b();
