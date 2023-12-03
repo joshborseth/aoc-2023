3d81b4c4a507c11dcef0e94fcab98daf6a5f86e0
joshborseth <joshuaborseth@gmail.com>
2023-12-03 15:05:10 -0800

    finished day3

--- puzzles/day-3/day-3-a.ts
+++ puzzles/day-3/day-3-a.ts
@@ -1,9 +1,67 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const check = (chars: (string[] | string)[]) => {
+  const flatChars = chars.flat();
+  const all = flatChars.filter((char) => {
+    if (!char) return false;
+    if (char === '.') return false;
+    if (isNaN(Number(char))) return true;
+  });
+  return all.length > 0;
+};
+
 export async function day3a(dataPath?: string) {
+  let count = 0;
   const data = await readData(dataPath);
-  return 0;
+  const globalArr = [];
+  data.forEach((line, i) => {
+    const splitLine = line.split('');
+    const usedIndexes = [];
+    splitLine.forEach((char, x) => {
+      if (usedIndexes.includes(x)) return;
+      if (char === '.') return;
+      if (!isNaN(Number(char))) {
+        let buildingNum = char;
+        for (let j = x + 1; j < splitLine.length; j++) {
+          if (isNaN(Number(splitLine[j]))) break;
+          buildingNum = `${buildingNum}${splitLine[j]}`;
+          usedIndexes.push(j);
+        }
+        globalArr.push(buildingNum);
+
+        const charLeft = splitLine[x - 1];
+        const charAfter = splitLine[x + buildingNum.length];
+        const charsAbove = data[i - 1]
+          ?.slice(x, x + buildingNum.length)
+          .split('');
+        const charsBelow = data[i + 1]
+          ?.slice(x, x + buildingNum.length)
+          .split('');
+
+        const charAboveLeft = data[i - 1]?.[x - 1];
+        const charAboveRight = data[i - 1]?.[x + buildingNum.length];
+        const charBelowLeft = data[i + 1]?.[x - 1];
+        const charBelowRight = data[i + 1]?.[x + buildingNum.length];
+
+        const isPartNumber = check([
+          charLeft,
+          charAfter,
+          charsAbove,
+          charsBelow,
+          charAboveLeft,
+          charAboveRight,
+          charBelowLeft,
+          charBelowRight,
+        ]);
+
+        if (isPartNumber) {
+          count += Number(buildingNum);
+        }
+      }
+    });
+  });
+  return count;
 }
 
 const answer = await day3a();
--- puzzles/day-3/day-3-b.ts
+++ puzzles/day-3/day-3-b.ts
@@ -2,8 +2,69 @@ import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
 export async function day3b(dataPath?: string) {
+  let count = 0;
   const data = await readData(dataPath);
-  return 0;
+  data.forEach((line, i) => {
+    const splitLine = line.split('');
+    splitLine.forEach((char, x) => {
+      if (char !== '*') return;
+
+      const findFullNum = ({ x, y }: { x: number; y: number }) => {
+        let char = data[y]?.[x];
+        if (!char) return;
+        if (isNaN(Number(char))) return;
+        for (let i = x; i < data[y].length; i++) {
+          if (i === x) continue;
+          let newChar = data[y][i];
+          if (!newChar) break;
+          if (isNaN(Number(newChar))) break;
+          char = `${char}${newChar}`;
+        }
+
+        for (let i = x; i >= 0; i--) {
+          if (i === x) continue;
+          let newChar = data[y][i];
+          if (!newChar) break;
+          if (isNaN(Number(newChar))) break;
+          char = `${newChar}${char}`;
+        }
+        return Number(char);
+      };
+      const numLeft = findFullNum({ y: i, x: x - 1 });
+      const numAfter = findFullNum({ y: i, x: x + 1 });
+      const numAbove = findFullNum({ y: i - 1, x });
+      const numBelow = findFullNum({ y: i + 1, x });
+      const numAboveLeft = findFullNum({ y: i - 1, x: x - 1 });
+      const numAboveRight = findFullNum({ y: i - 1, x: x + 1 });
+      const numBelowLeft = findFullNum({ y: i + 1, x: x - 1 });
+      const numBelowRight = findFullNum({ y: i + 1, x: x + 1 });
+
+      const nums = [
+        numLeft,
+        numAfter,
+        numAbove,
+        numBelow,
+        numAboveLeft,
+        numAboveRight,
+        numBelowLeft,
+        numBelowRight,
+      ];
+
+      const removeUndefined = nums.filter((num) => !!num);
+      const removeDuplicates = [...new Set(removeUndefined)];
+
+      if (removeDuplicates.length > 1) {
+        if (removeDuplicates.length !== 2)
+          throw new Error('numsToMultiply.length !== 2');
+        const gearProduct = removeDuplicates.reduce(
+          (acc, curr) => acc * curr,
+          1
+        );
+        count += Number(gearProduct);
+      }
+    });
+  });
+  return count;
 }
 
 const answer = await day3b();
