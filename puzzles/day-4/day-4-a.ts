228c7c7eebebc1ec8fe93716f90e01b15718b09b
joshborseth <joshuaborseth@gmail.com>
2023-12-04 08:39:46 -0800

    got day 4 finally, was way simpler than i was making it

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -3,7 +3,35 @@ import chalk from 'chalk';
 
 export async function day4a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let count = 0;
+  data.forEach((line) => {
+    const twoLines = line.split('|');
+    const colonIndex = twoLines[0].indexOf(':');
+    const winningNumbers = twoLines[0]
+      .slice(colonIndex + 1)
+      .trim()
+      .split(' ')
+      .filter((num) => !!num);
+    const myNumbers = twoLines[1]
+      .trim()
+      .split(' ')
+      .filter((num) => !!num);
+
+    const allNums = [...winningNumbers, ...myNumbers];
+    const allNumsSet = new Set(allNums);
+    const diffInSize = allNums.length - allNumsSet.size;
+    if (!diffInSize) return;
+
+    let total = 1;
+    new Array(diffInSize).fill(0).forEach((_, i) => {
+      if (i === 0) return;
+      total *= 2;
+    });
+
+    count += total;
+  });
+
+  return count;
 }
 
 const answer = await day4a();
--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -3,7 +3,29 @@ import chalk from 'chalk';
 
 export async function day4b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let copies = Array(data.length).fill(1);
+  data.forEach((line, i) => {
+    const twoLines = line.split('|');
+    const colonIndex = twoLines[0].indexOf(':');
+    const winningNumbers = twoLines[0]
+      .slice(colonIndex + 1)
+      .trim()
+      .split(' ')
+      .filter((num) => !!num);
+    const myNumbers = twoLines[1]
+      .trim()
+      .split(' ')
+      .filter((num) => !!num);
+
+    const allNums = [...winningNumbers, ...myNumbers];
+    const allNumsSet = new Set(allNums);
+
+    const winners = allNums.length - allNumsSet.size;
+
+    for (let j = 1; j <= winners; j++) copies[i + j] += copies[i];
+  });
+
+  return copies.reduce((acc, curr) => acc + curr, 0);
 }
 
 const answer = await day4b();
